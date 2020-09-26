03c19f47bfa99632a4b70461f83b63d488a69f73
ybZhang <472099249@qq.com>
2020-09-26 21:25:23 +0800

    通过components和views,完成组件拆分

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
-import Nav from './components/Nav'
+import Money from './views/Money'
+import Tags from './views/Tags'
+import Statistics from './views/Statistics'
+import NoMatch from './views/NoMatch'
 
 
 import {
@@ -10,57 +12,28 @@ import {
   Redirect
 } from "react-router-dom";
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display:flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags />
-            </Route>
-            <Route path="/statistics">
-              <Statistics />
-            </Route>
-            <Route path="/money">
-              <Money />
-            </Route>
-            <Redirect exact from="/" to="/money" />
-            <Route path="*">
-              <NoMatch />
-            </Route>
-          </Switch>
-        </Main>
-        <Nav />
-      </Wrapper>
+      <Switch>
+        <Route path="/tags">
+          <Tags />
+        </Route>
+        <Route path="/statistics">
+          <Statistics />
+        </Route>
+        <Route path="/money">
+          <Money />
+        </Route>
+        <Redirect exact from="/" to="/money" />
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
-function Money() {
-  return <h2>Money</h2>;
-}
-
-function Tags() {
-  return <h2>Tags</h2>;
-}
-
-function Statistics() {
-  return <h2>Statistics</h2>;
-}
-
-function NoMatch() {
-  return <p>当前页面不存在，请检查网址是否正确</p>;
-}
 
 export default App;
